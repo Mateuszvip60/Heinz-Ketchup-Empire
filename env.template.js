a9dde70fac1715f3027eb49039539edd5c280586
Mateuszvip25 <147926693+Mateuszvip60@users.noreply.github.com>
2025-10-14 00:58:03 +0200

    Delete env.template.js

--- env.template.js
+++
@@ -1,7 +0,0 @@
-window.__ENV__ = {
-    JSONSILO_KEY: "WPROWADŹ_SWÓJ_KLUCZ",
-    JSONSILO_FILE_ID: "82305321-d682-411d-b028-83640c37fc14",
-    JSONSILO_BASE_URL: "https://api.jsonsilo.com",
-    JSONSILO_REGION: "api",
-    ONLINE_FEATURES_ENABLED: "true"
-};
